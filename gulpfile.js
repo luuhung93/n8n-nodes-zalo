4e9dff2fe7b31e23baabd57f85af4e7b25d4e533
L Hung <luuhung93@gmail.com>
2025-06-10 23:31:26 +0700

    fix fullApiUrl

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const { task, src, dest } = require('gulp');
 task('build:icons', copyIcons);
 
 function copyIcons() {
-	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
+	const nodeSource = path.resolve('nodes', '**', '*.{png,svg,html,js,css}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
 	src(nodeSource).pipe(dest(nodeDestination));
 
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
+	const credSource = path.resolve('credentials', '**', '*.{png,svg,ts}');
 	const credDestination = path.resolve('dist', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
